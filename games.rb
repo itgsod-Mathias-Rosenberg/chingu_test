8e6c2112cf657c7aab1e940f007829634872188c
Mathias rosenberg <matrhias.rosenberg@elev.it-gymnasiet.se>
2012-11-23 10:46:46 +0100

    Fixed buggs and borders

--- games.rb
+++ games.rb
@@ -6,7 +6,7 @@ class Game < Chingu::Window
 	def initialize
 		super
 		self.input = {esc: :exit}
-		@Player = Player.create
+		@player = Player.create
 	end
 end
 
@@ -14,8 +14,39 @@ class Player < Chingu::GameObject
 
 	#meta-constructor
 	def setup
-		@x, @y = 350, 400
+		@x, @y = 750, 400
 		@image = Gosu::Image["camel.png"]
+		@speed = 4
+		self.input = {
+			holding_left: :left,
+			holding_right: :right,
+			holding_up: :up,
+			holding_down: :down
+		}
+	end
+
+	def left
+		unless @x - 70 <= 0
+			@x -= @speed
+		end
+	end
+
+	def right
+		unless @x + 70 >= 800
+			@x += @speed
+		end
+	end
+
+	def up 
+		unless @y - 70 <= 0
+			@y -= @speed
+		end
+	end
+
+	def down
+		unless @y + 70 >= 600
+			@y += @speed
+		end
 	end
 end
 
